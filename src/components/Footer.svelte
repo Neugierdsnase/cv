4d855d789ddcc81eee15bf6dfbb2aa7c6f43d73e
Konstantin Kovar <schwammerlkopf@hotmail.com>
2022-04-03 21:11:02 +0200

    added more text to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,14 +1,20 @@
-<footer class="mt-16 flex justify-around py-4 px-8">
-  <span>
-    E-Mail Adresse:
-    <a href="mailto:mail@vomkonstant.in"
-      >mail@vomkonstant.in</a
-    >
-  </span>
-  <span class="print:hidden">
-    <a
-      href="https://www.linkedin.com/in/konstantin-kovar-5301494b/"
-      >LinkedIn Profil</a
-    >
-  </span>
+<footer class="mt-16 flex flex-col items-center py-8 px-8">
+  <p class="mx-auto">
+    Gefällt Dir, was Du siehst? Ich bin offen für etwas
+    Neues! Kontaktiere mich:
+  </p>
+  <div class="flex justify-around">
+    <span>
+      E-Mail Adresse:
+      <a href="mailto:mail@vomkonstant.in"
+        >mail@vomkonstant.in</a
+      >
+    </span>
+    <span class="print:hidden">
+      <a
+        href="https://www.linkedin.com/in/konstantin-kovar-5301494b/"
+        >LinkedIn Profil</a
+      >
+    </span>
+  </div>
 </footer>
